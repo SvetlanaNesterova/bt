18d717a8931bc008d1dbab76d18b701f82f2488d
SvetlanaNesterova <nesterova.svetlanka@yandex.ru>
2017-12-19 20:03:15 +0500

    Started requests

--- downloader.py
+++ downloader.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import requests
 from  bencode_parser import BencodeParser
 
 class Loader:
@@ -23,12 +24,20 @@ class Loader:
 
     def download(self):
         self._check_file(self.torrent_file_path)
+        source = self._read_source_from_file()
+        content = BencodeParser.parse(source)
+        for key in content[0].keys():
+            print(key + ": " + str(content[0][key]))
+        self._get_peers(content[0])
+
+    def _read_source_from_file(self):
         # кодировка ? utf-8 полетела
         # UnicodeDecodeError: 'utf-8' codec can't decode byte 0xc0 in position 825: invalid start byte
         with open(self.torrent_file_path, 'rb') as meta:
             source = meta.read()
-            #print(source)
-            content = BencodeParser.parse(source)
-            for key in content[0].keys():
-                print(key + ": " + str(content[0][key]))
+        return source
 
+    def _get_peers(self, content):
+        tracker_url = content["announce"]
+
+        r = requests.get(tracker_url)
